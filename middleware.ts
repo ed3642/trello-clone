ab566ef5fa8d5016b97aec682b66b2324764b3d0
Eduardo San Martin Celi <ed3642dev@gmail.com>
2024-05-03 22:37:31 -0400

    organizations

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,30 @@
 import {
   clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+  createRouteMatcher,
+  redirectToSignIn
+} from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher([
-  '/protected',
+  '/organization(.*)',
+  '/select-org',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect() // Protect the routes
+  }
+
+  if (auth().userId && !isProtectedRoute(req)) {
+    let path = '/select-org'
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`
+    }
+
+    const orgSelection = new URL(path, req.url)
+    return NextResponse.redirect(orgSelection)
+  }
 });
 
 export const config = {
