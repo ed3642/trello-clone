9ce15bbf1c78bf942b2fc4e1acb1e960ca7793e8
Eduardo San Martin Celi <ed3642dev@gmail.com>
2024-05-05 01:06:39 -0400

    settings page

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -12,6 +12,7 @@ import {
 import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 export type Organization = {
@@ -107,5 +108,17 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  )
+}
+
+// nice way to have skeleton for components
+NavItem.Skeleton = function NavItemSkeleton() {
+  return (
+    <div className='flex items-center gap-x-2'>
+      <div className='w-10 h-10 relative shrink-0'>
+        <Skeleton className='h-full w-full absolute'/>
+      </div>
+      <Skeleton className='h-10 w-full'/>
+    </div>
   )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import {
   clerkMiddleware,
   createRouteMatcher,
-  redirectToSignIn
 } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
