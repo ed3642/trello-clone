7ece3f542b4f33b3d7b446701212babb164db471
Eduardo San Martin Celi <ed3642dev@gmail.com>
2024-05-08 22:07:26 -0400

    popover create board

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,25 +1,27 @@
-import { Button } from "@/components/ui/button"
-import { Logo } from "@/components/ui/logo"
-import Link from "next/link"
+import { Button } from '@/components/ui/button'
+import { Logo } from '@/components/logo'
+import Link from 'next/link'
 
 export const NavBar = () => {
   return (
-    <div className='fixed top-0 w-full h-14 px-4 border-b
-    shadow-sm bg-white flex items-center'>
-      <div className='md:max-w-screen-xl mx-auto flex items-center
-      w-full justify-between'>
+    <div
+      className="fixed top-0 w-full h-14 px-4 border-b
+    shadow-sm bg-white flex items-center"
+    >
+      <div
+        className="md:max-w-screen-xl mx-auto flex items-center
+      w-full justify-between"
+      >
         <Logo />
-        <div className='space-x-4 md:block md:w-auto flex
-        items-center justify-between w-full'>
-          <Button size='sm' variant='outline' asChild>
-            <Link href='/sign-in'>
-              Login
-            </Link>
+        <div
+          className="space-x-4 md:block md:w-auto flex
+        items-center justify-between w-full"
+        >
+          <Button size="sm" variant="outline" asChild>
+            <Link href="/sign-in">Login</Link>
           </Button>
-          <Button size='sm' asChild>
-            <Link href='/sign-up'>
-              Get Taskify for free
-            </Link> 
+          <Button size="sm" asChild>
+            <Link href="/sign-up">Get Taskify for free</Link>
           </Button>
         </div>
       </div>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,28 +5,28 @@ import { Poppins } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { Logo } from '@/components/ui/logo'
+import { Logo } from '@/components/logo'
 
 const headingFont = localFont({
-  src: '../../public/fonts/font.woff2'
+  src: '../../public/fonts/font.woff2',
 })
 
 const textFont = Poppins({
   subsets: ['latin'],
-  weight: [
-    '100', '200', '300', '400', '500', '600', '700', '800', '900', 
-  ],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className={cn(
-        "flex items-center justify-center flex-col",
-        headingFont.className
-      )}>
+      <div
+        className={cn(
+          'flex items-center justify-center flex-col',
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2"/>
+          <Medal className="h-6 w-6 mr-2" />
           No 1 Task Management App
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
@@ -36,17 +36,18 @@ const MarketingPage = () => {
           Work faster 🚀
         </div>
       </div>
-      <div className={cn(
-        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
-        textFont.className,
-        )}>
-        Collaborate, manage and track tasks with your team. Taskify is the best task management app for teams.
+      <div
+        className={cn(
+          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          textFont.className
+        )}
+      >
+        Collaborate, manage and track tasks with your team. Taskify is the best
+        task management app for teams.
       </div>
       {/* asChild makes sure the button works with the link */}
-      <Button className="mt-6" size="lg" asChild> 
-        <Link href="/sign-up">
-          Get Taskify for free!
-        </Link>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">Get Taskify for free!</Link>
       </Button>
     </div>
   )
